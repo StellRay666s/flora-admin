6befb1a417fb5f8e6076a080bfb0a734a3761305
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-10-06 20:06:13 +0300

    add axios

---
+++ utils/axiosClient.ts
@@ -0,0 +1,21 @@
+import axios, {AxiosRequestConfig}from 'axios'
+
+
+
+ const accessToken = window.localStorage.getItem("accessToken")
+
+
+const axiosClient = axios.create({
+    baseURL: process.env.REACT_APP_API_KEY,
+    timeout:99999
+
+})
+
+axiosClient.interceptors.request.use((config:AxiosRequestConfig) => {
+    if(config.headers){
+    config.headers.Authorization = accessToken
+    }
+    return config
+})
+
+export {axiosClient}
