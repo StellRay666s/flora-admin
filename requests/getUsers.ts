b153f58b945362d6ca446841b8257f1128b7f33b
Danila_Amazaryan <amazaryan2018@yandex.ru>
2022-10-13 23:40:29 +0300

    add bouquets page

--- requests/getUsers.ts
+++ requests/getUsers.ts
@@ -1,6 +1,6 @@
 import { axiosClient } from '@/utils/axiosClient'
 
-async function getUsers(skip: number = 10) {
+async function getUsers(skip: number = 0) {
   const response = await axiosClient.get<User[]>("/users",{
     params:{
         $skip:skip
